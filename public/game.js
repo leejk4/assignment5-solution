80a994008e6f2771254ef2dd95ce2cbfc5bcc92e
Sam Pepose <sampepose@gmail.com>
2016-10-31 21:39:14 -0500

    Added local game state data structure

--- public/game.js
+++ public/game.js
@@ -1,5 +1,30 @@
 'use strict';
 
+const NUM_PILES = 7;
+
+const constructKlondike = (cards) => {
+  const gameState = {
+    stack1: [],
+    stack2: [],
+    stack3: [],
+    stack4: [],
+    discard: [],
+  };
+  for (let i = 1; i < NUM_PILES + 1; i++) {
+    gameState[`pile${i}`] = [];
+    for (let j = 0; j < i; j++) {
+      const card = cards.pop();
+      card.up = (j == i - 1);
+      gameState[`pile${i}`].push(card);
+    }
+  }
+  gameState.draw = cards.map(card => {
+    card.up = false;
+    return card;
+  });
+  return gameState;
+};
+
 $(document).ready(function() {
   let startX, startY;
 
@@ -29,4 +54,18 @@ $(document).ready(function() {
   });
 
   $('img').draggable();
+
+  // Fetch random deck from server
+  $.ajax({
+    type: 'get',
+    url: '/v1/game/shuffle?jokers=false',
+    success: (cards) => {
+      const gameState = constructKlondike(cards);
+      console.log(gameState);
+      localStorage.setItem('gameState', JSON.stringify(gameState));
+    },
+    error: (xhr, status, error) => {
+
+    },
+  });
 });
